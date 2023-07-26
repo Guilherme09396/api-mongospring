4cd72e38efc22938052dc5c738bfbb595d27001d
Guilherme Gomes <guilherme09396@gmail.com>
2023-07-26 20:49:42 -0300

    Projeto iniciado

---
+++ src/test/java/com/guilherme/mongospring/MongospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.mongospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
